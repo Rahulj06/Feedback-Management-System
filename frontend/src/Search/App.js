c1aca183899a3180ab40b728f4d97d9d6fcc21e3
Rahulj06 <53989623+Rahulj06@users.noreply.github.com>
2021-06-22 18:52:48 +0530

    Add files via upload

---
+++ frontend/src/Search/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+//import './App.css';
+import SearchBar from "./MyComponents/SearchBar";
+function App() {
+  return (
+    <div className="App">
+      <SearchBar/>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ frontend/src/Search/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+// import * as serviceWorker from "./serviceWorker";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
