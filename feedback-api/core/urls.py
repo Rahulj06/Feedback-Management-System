5e2be8779c64407cc3d12ff9379d4aefe5852762
Rahulj06 <53989623+Rahulj06@users.noreply.github.com>
2021-06-23 17:54:25 +0530

    Delete feedback-api/core directory

--- feedback-api/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from core import views
-
-urlpatterns =[
-    url(r'^api/core$',views.core_list),
-    url(r'^api/core/(?P<pk>[0-9]+)$', views.core_detail),
-    url(r'^api/core/tag$', views.core_list_tag)
-]
